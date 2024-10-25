738644bcd9f3850aa4d8e1cd79b52b8633b2849e
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-10-25 17:19:13 +0530

    link attach in profile

--- frontend/src/components/TeamsDashboard.jsx
+++ frontend/src/components/TeamsDashboard.jsx
@@ -4,9 +4,12 @@ import PlayersByTeam from "./PlayersByTeam.jsx";
 
 const TeamsDashboard = () => {
   return (
-    <div className="flex   ">
-      
+    <div className="flex ">
+        
         <Sidebar />
+
+        
+         
       
          <div className="bg-slate-100 flex-initial w-64   md:w-96 lg:w-full mt-7 text-center ml-2 text-red-500 text-xl sm:text-2xl font-serif italic hover:not-italic">
       
