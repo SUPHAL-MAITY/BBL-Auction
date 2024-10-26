896b5cea9865c59d450d9396849f0df2cb78aac8
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-10-26 13:42:57 +0530

    playersByTeam allignment issue

--- frontend/src/components/PlayersByTeam.jsx
+++ frontend/src/components/PlayersByTeam.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import "../App.css";
 
 const PlayersByTeam = () => {
   const [players, setPlayers] = useState([]);
@@ -40,61 +41,40 @@ const PlayersByTeam = () => {
   };
 
   return (
-    <div className="bg-slate-100">
-      <div className="mx-16 my-4 ">
-        <img style={{ width: 100, height: 100 }} src={url} alt=" Logo" />
+   <>
+   <div className="pbt_img ">
+  <img  src={url} alt=" Logo" />
+</div>
+
+<div className="table__container">
+        <table className="table ">
+          <thead>
+            <tr>
+              <th className="head_attribute"> Sl</th>
+              <th className="head_attribute"> Name</th>
+              <th className="head_attribute">Role</th>
+              <th className="head_attribute">Sold Price</th>
+            </tr>
+          </thead>
+          {players.map((c, i) => {
+            
+
+            return (
+              <tbody className="row" key={i}>
+                <tr className="row_attribute">
+                  <td>{c.sl}</td>
+                  <td>{c.name}</td>
+                  <td>{c.role}</td>
+                  <td>{c.soldPrice}</td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
       </div>
-
-      <div className="flex flex-col">
-        <div className="-m-1.5 overflow-x-auto">
-          <div className="p-1 min-w-full inline-block align-middle">
-            <div className="overflow-hidden">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead>
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-1 text-start text-xs font-medium text-gray-500 uppercase"
-                    >
-                      Sl
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-1 text-start text-xs font-medium text-gray-500 uppercase"
-                    >
-                      Name
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="px-6 py-1 text-start text-xs font-medium text-gray-500 uppercase"
-                    >
-                      Sold Price
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {players.map((c, i) => (
-                    <tr className="odd:bg-white even:bg-gray-100" key={i}>
-                      <td className="px-6 py-1 whitespace-nowrap text-sm font-medium text-gray-800">
-                        {c.sl}
-                      </td>
-                      <td className="px-6 py-1 whitespace-nowrap text-sm font-medium text-gray-800">
-                        {c.name}
-                      </td>
-
-                      <td className="px-6 py-1 whitespace-nowrap text-sm font-medium text-gray-800">
-                        {c.soldPrice}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+   
+   
+   </>
   );
 };
 
